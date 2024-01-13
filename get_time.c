18d791802f2c2036b63091d52b4c4330c3372363
Ximer <20438890+Ximer612@users.noreply.github.com>
2024-01-13 18:25:52 +0100

    Fixed bug due to big endian numbers used with little endian functions

--- get_time.c
+++ get_time.c
@@ -15,20 +15,43 @@
 
 typedef struct ntp_packet
 {
-    const unsigned char leap_version_mode;
-    const unsigned char stratum;
-    const unsigned char poll;
-    const unsigned char precision;
-    const unsigned int root_delay;
+    unsigned char leap_version_mode;
+    unsigned char stratum;
+    unsigned char poll;
+    unsigned char precision;
+    unsigned int root_delay;
 
-    const unsigned int root_dispersion;
-    const char rfid[4];
+    unsigned int root_dispersion;
+    char rfid[4];
 
-    const unsigned long long ref_timestamp;
-    const unsigned long long original_timestamp;
-    const unsigned long long receive_timestamp;
-    const unsigned long long transmit_timestamp;
+    unsigned long long ref_timestamp;
+    unsigned long long original_timestamp;
+    unsigned long long receive_timestamp;
+    unsigned long long transmit_timestamp;
 } ntp_packet;
+
+unsigned int to_le_int(const unsigned int value)
+{
+    unsigned int byte0 = value << 24;
+    unsigned int byte1 = (value & 0xFF00) << 8;
+    unsigned int byte2 = (value & 0xFF0000) >> 8;
+    unsigned int byte3 = (value & 0xFF000000) >> 24;
+
+    return byte0 | byte1 | byte2 | byte3;
+}
+
+unsigned long long to_le_long(const unsigned long long value) {
+    unsigned long long byte0 = value << 56;
+    unsigned long long byte1 = (value & 0xFF00) << 40;
+    unsigned long long byte2 = (value & 0xFF0000) << 24;
+    unsigned long long byte3 = (value & 0xFF000000) << 8;
+    unsigned long long byte4 = (value & 0xFF00000000) >> 8;
+    unsigned long long byte5 = (value & 0xFF0000000000) >> 24;
+    unsigned long long byte6 = (value & 0xFF000000000000) >> 40;
+    unsigned long long byte7 = (value >> 56);
+
+    return byte0 | byte1 | byte2 | byte3 | byte4 | byte5 | byte6 | byte7;
+}
 
 void print_ntp_packet(ntp_packet packet)
 {
@@ -38,28 +61,11 @@ void print_ntp_packet(ntp_packet packet)
     MAGENTA_PRINT("precision => %d", packet.precision);
     MAGENTA_PRINT("root_delay => %d", packet.root_delay);
     MAGENTA_PRINT("root_dispersion => %d", packet.root_dispersion);
-    MAGENTA_PRINT("reference identifier => %s", packet.rfid);
+    MAGENTA_PRINT("reference identifier => %.4s", packet.rfid);
     MAGENTA_PRINT("reference timestamp => %llu", packet.ref_timestamp);
     MAGENTA_PRINT("original timestamp => %llu", packet.original_timestamp);
     MAGENTA_PRINT("receive timestamp => %llu", packet.receive_timestamp);
-    MAGENTA_PRINT("transmit timestamp => %llu", packet.transmit_timestamp);
-}
-
-void print_ntp_packet_inline(ntp_packet packet)
-{
-    SET_BLUE_PRINT();
-    printf("Leap => %d, Version => %d, Mode => %d", packet.leap_version_mode >> 6, packet.leap_version_mode >> 3 & 0xFFF, packet.leap_version_mode & 0xF);
-    printf(", stratum => %d", packet.stratum);
-    printf(", poll => %d", packet.poll);
-    printf(", precision => %d", packet.precision);
-    printf(", root_delay => %d", packet.root_delay);
-    printf(", root_dispersion => %d", packet.root_dispersion);
-    printf(", reference identifier => %s", packet.rfid);
-    printf(", reference timestamp => %llu", packet.ref_timestamp);
-    printf(", original timestamp => %llu", packet.original_timestamp);
-    printf(", receive timestamp => %llu", packet.receive_timestamp);
-    printf(", transmit timestamp => %llu \n", packet.transmit_timestamp);
-    SET_DEFAULT_PRINT();
+    MAGENTA_PRINT("transmit timestamp => %llu",packet.transmit_timestamp);
 }
 
 int main(int argc, char **argv)
@@ -107,25 +113,45 @@ int main(int argc, char **argv)
     
     struct sockaddr_in sender;
     int sender_len = sizeof(sender);
-    size_t received_bytes = recvfrom(s, (char*)&recv_packet, sizeof(recv_packet), 0,
+    size_t received_bytes = recvfrom(s, (char*)&recv_packet, sizeof(ntp_packet), 0,
                                       (struct sockaddr*)&sender, &sender_len);
+
+    recv_packet.root_delay = to_le_int(recv_packet.root_delay);
+    recv_packet.root_dispersion = to_le_int(recv_packet.root_dispersion);
+    recv_packet.ref_timestamp = to_le_long(recv_packet.ref_timestamp);
+    recv_packet.original_timestamp = to_le_long(recv_packet.original_timestamp);
+    recv_packet.receive_timestamp = to_le_long(recv_packet.receive_timestamp);
+    recv_packet.transmit_timestamp = to_le_long(recv_packet.transmit_timestamp);
+    
     print_ntp_packet(recv_packet);
 
     char outstr[128];
-    time_t t;
     struct tm tmp;
 
     localtime_s(&tmp,&now);
     strftime(outstr, sizeof(outstr), "%a, %d %b %Y %T %z", &tmp);
     YELLOW_PRINT("My computer's today is %s", outstr);
 
-    time_t recv_time = (time_t)((recv_packet.receive_timestamp >> 32));
+    int leap_years_count = (tmp.tm_year - 55)/4; //1954
+    time_t recv_time = (time_t)((recv_packet.receive_timestamp >> 32)  - ((unsigned int)365 * 3600 * 24 * 70) - ((unsigned int)leap_years_count * 3600 * 24));
 
     localtime_s(&tmp,&recv_time);
+
     strftime(outstr, sizeof(outstr), "%a, %d %b %Y %T %z", &tmp);
     YELLOW_PRINT("Server google's today is %s", outstr);
 
-    //Python result => (36, 1, 0, 236, 0, 7, b'GOOG', 16810710409316310953, 0, 16810710409316310954, 16810710409316310956)
+    //Debug create file
+    FILE* file_to_write;
+    fopen_s(&file_to_write,"ntp_packet.txt", "w");
+
+    if(file_to_write == NULL)
+    {
+        printf("Unable to create file.\n");
+        return -1;
+    }
+
+    fwrite((char*)&recv_packet,sizeof(char),sizeof(ntp_packet),file_to_write);
+    fclose(file_to_write);
 
     return 0;
 }
